69445bfcdd845caf15ab19e82ed9b56ca9044616
elzeresdai <elzeresdai@gmail.com>
2024-06-20 03:01:44 +0300

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load environment variables from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file, using default port.")
+	}
+
+}
